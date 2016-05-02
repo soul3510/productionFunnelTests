f5caefbce3e41fed26de836dac5ddcdb899e4401
Eyal Sooliman <soul3510@gmail.com>
2016-05-02 13:53:47 +0300

    Starting point

---
+++ src/test/java/com/tf/qa/production/tests/SampleTest.java
@@ -0,0 +1,12 @@
+package com.tf.qa.production.tests;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+
+    @Test(description = "Sample Test", enabled = true)
+    public void checkChangePasswordSuccessful() throws Exception {
+
+    }
+}
---
+++ src/test/java/com/tf/qa/production/utilities/ValidateUserMessage.java
@@ -0,0 +1,17 @@
+package com.tf.qa.production.utilities;
+
+import com.gargoylesoftware.htmlunit.ElementNotFoundException;
+
+/**
+ * Created by Eyal on 25/11/2015.
+ */
+public class ValidateUserMessage {
+
+    /**
+     * This is a sample method
+     * @throws ElementNotFoundException
+     */
+    public static void sample() throws ElementNotFoundException {
+
+    }
+}
